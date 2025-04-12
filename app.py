cf5d0e01c3d87855837491a29966d4f8b371817c
Tanisha-vernekar <160761378+Tanisha-vernekar@users.noreply.github.com>
2025-04-12 19:52:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess the image
     img = image.resize((128, 128))  # use the size your model was trained on
